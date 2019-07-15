525c6a0483ae31bbb46f08276a2f10a1bf0fa223
AlessanderTASC6 <attu72289@gmail.com>
2019-07-15 08:52:27 -0400

    mariopaintmvp

---
+++ Day0/intro.js
@@ -0,0 +1,12 @@
+function setup(){
+    createCanvas(600,500)
+    background(255,0,0)
+
+}
+
+
+function draw(){
+
+    circle(100, 100, 80);
+
+}
---
+++ Day0/mariopaintmvp/sketch.js
@@ -0,0 +1,16 @@
+function setup(){
+
+    createCanvas(500,500,0)
+}
+
+function draw(){
+
+ellipse(50,50,100)
+
+}
+
+function mouseDragged(){
+
+
+
+}
