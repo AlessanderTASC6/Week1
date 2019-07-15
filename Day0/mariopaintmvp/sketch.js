46534cc07f950215df4c6edcc40ac83f170ccd09
AlessanderTASC6 <attu72289@gmail.com>
2019-07-15 10:57:53 -0400

    funcion mousedDragged added

--- Day0/mariopaintmvp/sketch.js
+++ Day0/mariopaintmvp/sketch.js
@@ -1,16 +1,25 @@
+let value = 0;
+
 function setup(){
 
-    createCanvas(500,500,0)
+    createCanvas(500,500);
+    // background(0,0,0)
+
 }
 
 function draw(){
 
-ellipse(50,50,100)
+ellipse(random([value]),random([value]),value)
 
 }
 
-function mouseDragged(){
+function mouseDragged() {
+    value = value + 5;
+    if (value > 255) 
+      value = 0;
+    }
 
 
 
-}
+
+
